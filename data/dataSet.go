16158b9f5959de0a779a9e11ca05cb4fcbfa3b4e
76040320K <rlatmdgh567@naver.com>
2022-02-19 22:50:13 +0900

    dataSet modified

--- data/dataSet.go
+++ data/dataSet.go
@@ -16,5 +16,21 @@ var (
 			Idx: 3,
 			Name: "three",
 		},
+		{
+			Idx: 4,
+			Name: "four",
+		},
+		{
+			Idx: 5,
+			Name: "five",
+		},
+		{
+			Idx: 6,
+			Name: "six",
+		},
+		{
+			Idx: 7,
+			Name: "seven",
+		},
 	}
 )
